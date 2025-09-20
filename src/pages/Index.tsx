85688860bbe58fe368c15a5a62d43f09569a1228
roshanyadav-2109 <businessmarketing0002@gmail.com>
2025-09-21 01:37:17 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,6 @@
 import { Header } from "@/components/header";
-import { HeroSection } from "@/components/hero-section";  
+import { HeroSection } from "@/components/hero-section";
 import { ProblemSection } from "@/components/problem-section";
-import { FeaturesSection } from "@/components/features-section";
-import { ProductGallerySection } from "@/components/product-gallery-section";
 import { TestimonialsSection } from "@/components/testimonials-section";
 import { FaqSection } from "@/components/faq-section";
 import { CtaSection } from "@/components/cta-section";
@@ -14,8 +12,6 @@ const Index = () => {
       <main>
         <HeroSection />
         <ProblemSection />
-        <FeaturesSection />
-        <ProductGallerySection />
         <TestimonialsSection />
         <FaqSection />
         <CtaSection />
